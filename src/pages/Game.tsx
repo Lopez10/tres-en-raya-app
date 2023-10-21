0a571085953f51deacfbffd41424bf845d767360
Juan LB <ljuan7397@gmail.com>
2023-10-21 14:59:42 +0200

    Add Board to Game page

--- src/pages/Game.tsx
+++ src/pages/Game.tsx
@@ -1,9 +1,10 @@
+import { Board } from "../components/board/Board";
 import { Container } from "../components/container/Container";
 
 export function Game(): JSX.Element {
     return (
         <Container title='Game'>
-            <div></div>
+            <Board />
         </Container>
     )
 }
