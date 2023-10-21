7b18e73c57c51edeeb90aa3575c20be60745809f
Juan LB <ljuan7397@gmail.com>
2023-10-21 10:33:58 +0200

    Create POST game methods

--- src/services/game.ts
+++ src/services/game.ts
@@ -6,3 +6,15 @@ export type Game = {
     playerId: string;
     winner: string;
 };
+
+const post = async (url: string, data: unknown) => {
+    await fetch(url, {
+        method: 'POST',
+        body: JSON.stringify(data),
+        headers: { 'Content-Type': 'application/json' },
+    });
+}
+
+export const createGame = (playerId: string) => post('http://localhost:3000/games', { playerId })
+
+export const moveBoard = (game: Game) => post('http://localhost:3000/games/move', { ...game })
