f0f6a0dcbc0da0d0a727d77a481a6c337490f7eb
Juan LB <ljuan7397@gmail.com>
2023-10-21 11:52:55 +0200

    Add Container to Home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,12 @@
+import { Container } from "../components/container/Container";
 import { NewPlayerForm } from "../components/new-player-form/NewPlayerForm";
 
 export function Home(): JSX.Element {
     return (
         <div>
-            <NewPlayerForm onSubmit={() => { }} />
+            <Container title='Home'>
+                <NewPlayerForm onSubmit={() => { }} />
+            </Container>
         </div>
     )
 }
