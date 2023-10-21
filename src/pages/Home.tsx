9c948151095f42ce8c7b250f90c0168b7757d32b
Juan LB <ljuan7397@gmail.com>
2023-10-21 23:15:48 +0200

    Save player in localStorage

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,12 @@
 import { Container } from "../components/container/Container";
 import { NewPlayerForm } from "../components/new-player-form/NewPlayerForm";
 import { Player, newPlayer } from "../services/player";
+import { savePlayer } from "../storage";
 
 export function Home(): JSX.Element {
     async function handleSubmitNewPlayer(username: string) {
         const player: Player = await newPlayer(username);
-        console.log(player)
-        window.localStorage.setItem('player', JSON.stringify(player));
+        savePlayer(player);
     }
 
     return (
