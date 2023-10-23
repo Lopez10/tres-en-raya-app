6856419f815e3c49f38b81f0c7b762cd505ca641
Juan LB <ljuan7397@gmail.com>
2023-10-23 20:22:44 +0200

    Move route items

--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -1,19 +1,72 @@
 import { Route, BrowserRouter, Routes } from "react-router-dom";
-import { ROUTE_ITEMS } from "./RouteItems";
-import { NavBar } from "./navbar/NavBar";
-
+import { NavBar } from "../components/navbar/NavBar";
+import { getPlayer } from "../storage";
+import { ProtectedRoute } from "../components/route/ProtectedRoute";
+import { Game } from "../pages/Game";
+import { Ranking } from "../pages/Ranking";
+import { Home } from "../pages/Home";
+import { useEffect, useState } from "react";
+import { Player } from "../services/player";
 
 export function Router(): JSX.Element {
+    const [player, setPlayer] = useState<Player | null>(null);
+
+    useEffect(() => {
+        const player = getPlayer();
+        setPlayer(player);
+    }, []);
+
     return (
         <BrowserRouter>
-            <NavBar />
+            <NavBar routes={ROUTE_ITEMS} />
             <Routes>
-                {
-                    ROUTE_ITEMS.map((route, index) =>
-                        <Route key={index} path={route.path} element={route.element} />
-                    )
-                }
+                {ROUTE_ITEMS.map(({ path, element, logged }) => (
+                    <Route
+                        key={path}
+                        path={path}
+                        element={
+                            logged ? (
+                                <ProtectedRoute
+                                    redirectTo="/"
+                                    isAllowed={!!player}
+                                >
+                                    {element}
+                                </ProtectedRoute>
+                            ) : (
+                                element
+                            )
+                        }
+                    />
+                ))}
             </Routes>
         </BrowserRouter>
     )
 }
+
+const ROUTE_ITEMS: routeItems = [
+    {
+        name: 'Home',
+        path: '/',
+        element: <Home />,
+        logged: false
+    },
+    {
+        name: 'Ranking',
+        path: '/ranking',
+        element: <Ranking />,
+        logged: true
+    },
+    {
+        name: 'Game',
+        path: '/game',
+        element: <Game />,
+        logged: true
+    }
+];
+
+export type routeItems = {
+    name: string;
+    path: string;
+    element: JSX.Element;
+    logged: boolean;
+}[]
