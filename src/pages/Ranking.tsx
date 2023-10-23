bfc8d749706fe94574d456f5a9b8f36315dd7faa
Juan LB <ljuan7397@gmail.com>
2023-10-23 18:59:43 +0200

    Add result text to ranking

--- src/pages/Ranking.tsx
+++ src/pages/Ranking.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import { Container } from "../components/container/Container";
 import { Player, emptyPlayer, newPlayer } from "../services/player";
 import { getPlayer } from "../storage";
+import { ResultText } from "../components/text/ResultText";
 
 export function Ranking(): JSX.Element {
     const [player, setPlayer] = useState<Player>(emptyPlayer);
@@ -25,8 +26,9 @@ export function Ranking(): JSX.Element {
         <Container>
             <div>
                 <h1>{player.username}</h1>
-                <h2>{player.wins}</h2>
-                <h2>{player.losses}</h2>
+                <ResultText text="Wins" result={player.wins} />
+                <ResultText text="Losses" result={player.losses} />
+                <ResultText text="Draws" result={player.draws} />
             </div>
         </Container>
     )
